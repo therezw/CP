7daee2659ba5e69a6d7b328a63cd1e099ce26664
Reza Abedi <115654202+therezw@users.noreply.github.com>
2022-11-07 01:42:41 +0330

    Add files via upload

---
+++ p41.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+// H/W _ Week4 _ 40113841054003 Reza Abedi
+
+// exs1
+
+using namespace std;
+int main() {
+	cout << "Reza Abedi ";
+		cout<< endl;
+	float  num1=9.5;
+	float  num2=3.5;
+	int num5= 9;
+	int num6= 3;
+	cout << num1 + num2 ;
+		cout<< endl;
+		cout << num1 - num2;
+		cout<< endl;
+		cout << num1 * num2;
+		cout<< endl;
+		cout << num5 % num6;
+		cout<< endl;
+}       
---
+++ p42.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+// H/W _ Week4 _ 40113841054003 Reza Abedi
+// exs2
+using namespace std;
+
+int main() {
+	int x,y,z;
+	x = x-y;
+	cout << x;
+	cout<< endl;
+	y = x+z;
+	cout << y;
+	cout << endl;
+	z =  x * y;
+	cout << z;
+	cout << endl;
+x = (x+y+z);
+cout << x;
+cout<< endl;
+z= x*2;
+cout << z;
+cout << endl;
+y= z+1;
+cout << y ;
+cout << endl;
+x = z/y;
+cout << x;
+	
+}
