28dad877b7fde9521804e695ae8d8dfe85dd72b6
Reza Abedi <115654202+therezw@users.noreply.github.com>
2022-11-13 22:13:04 +0330

    Add files via upload

---
+++ p54.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+// H/W _ Week5 _ 40113841054003 Reza Abedi
+// exs 2
+using namespace std;
+
+int main() {
+	
+	char Ra;
+	
+cout<<"please Enter an Ra Word;";
+
+cin>>Ra;
+
+switch (Ra){
+	
+	case 'r':
+	cout<<"VOWEL";
+	break;
+	
+	case'e':
+	cout<<"VOWEL";
+	break;
+	
+	case'z':
+	cout<<"VOWEL";
+	break;
+	
+	case'a':
+	cout<<"VOWEL";
+	break;
+	
+	case'w':
+	cout<<"VOWEL";
+	break;	
+}
+}
